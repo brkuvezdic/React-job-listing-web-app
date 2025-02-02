5f4f897e8ab9d3bfd00cb56ad3a74900a7b24748
brkuvezdic <branimirkuvezdic@gmail.com>
2025-02-02 12:40:52 +0100

    Show more/less description

--- React jobs posting/src/components/JobListing.jsx
+++ React jobs posting/src/components/JobListing.jsx
@@ -17,13 +17,15 @@ const JobListing = ({ job }) => {
             <div className="text-gray-600 my-2">{job.type}</div>
             <h3 className="text-xl font-bold">{job.title}</h3>
           </div>
-
           <div className="mb-5">{description}</div>
-
+          <button
+            onClick={() => setShowFullDescription(!showFullDescription)}
+            className="text-indigo-500 mb-5 hover:text-indigo-600"
+          >
+            {showFullDescription ? "Less" : "More"}
+          </button>
           <h3 className="text-indigo-500 mb-2">{job.salary} / year</h3>
-
           <div className="border border-gray-100 mb-5"></div>
-
           <div className="flex flex-col lg:flex-row justify-between mb-4">
             <div className="text-orange-700 mb-3">
               <i className="fa-solid fa-location-dot text-lg"></i>
