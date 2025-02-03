c8b9c554f92b3a8fd28b605754737bc527ce20fb
brkuvezdic <branimirkuvezdic@gmail.com>
2025-02-03 20:51:39 +0100

    Display title of selected job

--- React jobs posting/src/pages/JobPage.jsx
+++ React jobs posting/src/pages/JobPage.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 import React from "react";
 
 const JobPage = () => {
-  return <div>JobPage</div>;
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        if (!res.ok) throw new Error("Failed to fetch job");
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJob();
+  }, []);
+
+  return loading ? (
+    <Spinner />
+  ) : job ? (
+    <h1>{job.title}</h1>
+  ) : (
+    <p>Job not found</p>
+  );
 };
 
 export default JobPage;
